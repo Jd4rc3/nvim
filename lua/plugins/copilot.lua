7def8697268aeb3750ab976ae20b11d2cb44d7f5
Juan Daniel <jdarce91@misena.edu.co>
2024-01-02 19:15:58 -0500

    trouble, neogen, copilot, cloak

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+  "github/copilot.vim",
+  config = function()
+    vim.cmd [[let g:copilot_no_tab_map = v:true]]
+    vim.cmd [[imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")]]
+    vim.cmd [[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
+    vim.cmd [[let g:copilot_filetypes = { '.env': v:false, }
+    ]]
+  end
+}
