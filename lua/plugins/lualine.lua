66e8a873fd914f9e2fd33fd2565693b275a05ad9
Juan Daniel <jdarce91@misena.edu.co>
2024-01-14 11:22:16 -0500

    lua linea customize

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,10 +4,18 @@ return {
   -- See `:help lualine.txt`
   opts = {
     options = {
-      icons_enabled = false,
+      icons_enabled = true,
       theme = 'catppuccin',
       component_separators = '|',
       section_separators = '',
+      always_divide_middle = true,
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics', 'filename' },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
     },
   },
 }
