87ea6ff28fb1ad4bbe03568d0089a38de7aeaa58
Juan Daniel <jdarce91@misena.edu.co>
2024-01-14 19:21:28 -0500

    none-ls config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,16 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.completion.spell,
+        null_ls.builtins.code_actions.refactoring,
+        --  golang
+        null_ls.builtins.code_actions.gomodifytags,
+        null_ls.builtins.code_actions.impl,
+        null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.diagnostics.gospel,
+        null_ls.builtins.formatting.gofmt
       },
     }
   end,
