3fef6a6297aaebff3a8d016573ce25b85cb43927
Juan Daniel <jdarce91@misena.edu.co>
2024-01-22 12:03:39 -0500

    Replace current word (meta-s)

--- lua/arce/remap.lua
+++ lua/arce/remap.lua
@@ -64,7 +64,7 @@ vim.keymap.set('n', '<M-j>', '<cmd>cprev<CR>zz', { desc = 'Previous in QuickFix'
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace current word (Using substituve)' })
+vim.keymap.set('n', '<M-s>', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace current word (Using substituve)' })
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/appdata/local/nvim/lua/arce/packer.lua<CR>");
