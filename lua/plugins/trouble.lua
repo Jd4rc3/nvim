f0af40faa01ebffbd3abfa060edb4113fc91cf99
Juan Daniel <jdarce91@misena.edu.co>
2024-02-01 15:52:16 -0500

    compiler & trouble config

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -58,34 +58,42 @@ return {
   },
   config = function()
     -- Lua
+    local trouble = require 'trouble'
+
     vim.keymap.set('n', '<leader>xx', function()
-      require('trouble').toggle()
+      trouble.toggle()
     end, { desc = 'toggle trouble list' })
 
     vim.keymap.set('n', '<leader>xw', function()
-      require('trouble').toggle 'workspace_diagnostics'
+      trouble.toggle 'workspace_diagnostics'
     end, { desc = ' toggle workspace diagnostics' })
 
     vim.keymap.set('n', '<leader>xd', function()
-      require('trouble').toggle 'document_diagnostics'
+      trouble.toggle 'document_diagnostics'
     end, { desc = 'toggle document diagnostics' })
 
     vim.keymap.set('n', '[d', function()
-      require('trouble').next { skip_groups = true, jump = true }
+      if not trouble.is_open() then
+        trouble.toggle()
+      end
+      trouble.next { skip_groups = true, jump = true }
     end, { desc = 'Go to previous diagnostic message' })
 
     vim.keymap.set('n', ']d', function()
-      require('trouble').previous { skip_groups = true, jump = true }
+      if not trouble.is_open() then
+        trouble.toggle()
+      end
+      trouble.previous { skip_groups = true, jump = true }
     end, { desc = 'Go to next diagnostic message' })
 
     vim.keymap.set('n', '<leader>xq', function()
-      require('trouble').toggle 'quickfix'
+      trouble.toggle 'quickfix'
     end, { desc = 'toggle quickfix' })
     vim.keymap.set('n', '<leader>xl', function()
-      require('trouble').toggle 'loclist'
+      trouble.toggle 'loclist'
     end, { desc = 'toggle loclist' })
     vim.keymap.set('n', 'gR', function()
-      require('trouble').toggle 'lsp_references'
+      trouble.toggle 'lsp_references'
     end, { desc = 'toggle lsp references' })
   end,
 }
