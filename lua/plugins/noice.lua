ade08f19c7ae4aa801265ded20de5bbbd7496ca0
Juan Daniel <jdarce91@misena.edu.co>
2024-02-10 15:03:54 -0500

    noice notify removed

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,7 +5,7 @@ return {
   event = 'VeryLazy',
   dependencies = {
     'MunifTanjim/nui.nvim',
-    -- 'rcarriga/nvim-notify',
+    'rcarriga/nvim-notify',
   },
   config = function()
     require('noice').setup {
@@ -34,9 +34,9 @@ return {
         -- NOTE: If you enable messages, then the cmdline is enabled automatically.
         -- This is a current Neovim limitation.
         enabled = false, -- enables the Noice messages UI
-        view = 'notify', -- default view for messages
-        view_error = 'notify', -- view for errors
-        view_warn = 'notify', -- view for warnings
+        view = 'messages', -- default view for messages
+        view_error = 'messages', -- view for errors
+        view_warn = 'messages', -- view for warnings
         view_history = 'messages', -- view for :messages
         view_search = 'virtualtext', -- view for search count messages. Set to `false` to disable
       },
