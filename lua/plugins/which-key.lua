a3c592029fc14b491563460df381468608762d99
Daniel <juandanielarce398@gmail.com>
2024-08-12 20:31:57 -0500

    Update which key config to new api

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,35 @@
 -- Useful plugin to show you pending keybinds.
 return {
   'folke/which-key.nvim',
+  dependencies = {
+    {
+      'echasnovski/mini.nvim',
+      config = function()
+        local MiniIcons = require 'mini.icons'
+        MiniIcons.setup()
+      end,
+    },
+  },
   opts = {},
   config = function()
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    local wk = require 'which-key'
+    wk.setup()
+    wk.add {
+      { '<leader>g', group = '[G]it' }, -- group
+      { '<leader>h', group = '[H]unk' }, -- group
+      { '<leader>r', group = '[R]ename' }, -- group
+      { '<leader>s', group = '[S]earch' }, -- group
+      { '<leader>t', group = '[T]oggle' }, -- group
+      { '<leader>w', group = '[W]orkspace' }, -- group
+      { '<leader>d', group = '[D]ocument' }, -- group
+      { '<leader>c', group = '[C]ode' }, -- group
+      {
+        '<leader>b',
+        group = 'buffers',
+        expand = function()
+          return require('which-key.extras').expand.buf()
+        end,
+      },
     }
-    -- register which-key VISUAL mode
-    -- required for visual <leader>hs (hunk stage) to work
-    require('which-key').register({
-      ['<leader>'] = { name = 'VISUAL <leader>' },
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
   end,
 }
